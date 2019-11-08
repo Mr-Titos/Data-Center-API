4ee673213cb7944a7e025bd24dab8aac9aaa5128
Mr-Titos <arthur.titos@gmail.com>
2019-11-08 10:37:59 +0100

    add save method to score.json

--- program.js
+++ program.js
@@ -2,20 +2,35 @@ var http = require('http');
 var fs = require('fs');
 
 var tabData;
+var reqBody = "";
 
 http.createServer(function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'method, file');
     var indexM = req.rawHeaders.indexOf("method");
     var indexF = req.rawHeaders.indexOf("file");
     if(indexM != -1){
         if(indexF != -1) {
-            loadFile(req.rawHeaders[indexF + 1]).then(() => {
-                res.write(responseJson(tabData,req.rawHeaders[indexM + 1],req.rawHeaders[indexF + 1]));
-                console.log("request " + req.rawHeaders[indexM + 1] + " on the file : " + req.rawHeaders[indexF + 1]);
-                res.end();
-            }).catch(err => {
-                res.write(err);
-                res.end();
-            })
+            if (req.method === 'POST' && req.rawHeaders[indexM + 1] == "save") {
+                req.on('data', chunk => {
+                    reqBody = chunk.toString(); // convert Buffer to string
+                });
+                req.on('end', () => {
+                    res.write(responseJson(JSON.parse(reqBody),req.rawHeaders[indexM + 1],req.rawHeaders[indexF + 1]));
+                    console.log("request " + req.rawHeaders[indexM + 1] + " on the file : " + req.rawHeaders[indexF + 1]);
+                    res.end();
+                });
+            } else {
+                loadFile(req.rawHeaders[indexF + 1]).then(() => {
+                    res.write(responseJson(tabData,req.rawHeaders[indexM + 1],req.rawHeaders[indexF + 1]));
+                    console.log("request " + req.rawHeaders[indexM + 1] + " on the file : " + req.rawHeaders[indexF + 1]);
+                    res.end();
+                }).catch(err => {
+                    res.write(err);
+                    res.end();
+                })
+            }
         } else {
             res.write("Error, No File name found");
             res.end();
@@ -42,9 +57,20 @@ function loadFile(name) {
                     tabData = JSON.parse(jsonString)
                     res();
             } catch(err) {
-                    rej('Error parsing ' + name + ' JSON string:' + err.message + "\nBe sure its a JSON file");
+                    rej('Error parsing ' + name + ' JSON string : ' + err.message + "\nBe sure its a JSON file");
                 } 
             }
+        });
+    })
+}
+
+function writeToFile(name, tablal) {
+    return new Promise(function(res,rej) {
+        fs.writeFile("data/" + name, tablal, (err) => {
+            if (err) {
+                rej(name + " file writing failed:" + err.message);
+            } else 
+                res();
         });
     })
 }
@@ -61,11 +87,19 @@ function responseJson(tablal, method, name) {
                     return "An error has been found in the method";
             }
         case 'score.json' :
-                switch(method) {
-                    case 'all' :
-                        return JSON.stringify(tablal); 
-                    default:
-                        return "An error has been found in the method";
+            switch(method) {
+                case 'all' :
+                    return JSON.stringify(tablal); 
+                case 'save' :
+                    writeToFile("score.json", JSON.stringify(tablal, null, 2)).then(() => {
+                        console.log("Written success");
+                        return "Succesfully written !";
+                    }).catch(err => { 
+                        console.log("Err Write" + err);
+                        return "Error in the writing of file !";
+                    });
+                default:
+                    return "An error has been found in the method";
                 }
         default:
             return "An error has been found in the file name";

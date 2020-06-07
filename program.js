df4024ce80142b3830cdcaa2b3a18776989cef66
Mr-Titos <Arthur.Titos2@gmail.com>
2020-06-07 21:15:49 +0200

    add La_Boule data

--- program.js
+++ program.js
@@ -50,20 +50,39 @@ function processRequest(req, res, indexMethod, indexFile, paramRequest) {
     return new Promise((resolve, reject) => {
         let methodName = req.rawHeaders[indexMethod + 1];
         let fileName = req.rawHeaders[indexFile + 1];
-        if (req.method === 'POST') {
+        if (req.method === 'POST') { // Method POST
             switch (methodName) {
                 case 'save':
                     req.on('data', (chunk) => {
                         reqBody.push(chunk);
                     }).on('end', () => {
-                        getUpdatedGoboueArray(fileName, Buffer.concat(reqBody).toString()).then(updatedArr => {
+                        addToGoboueArray(fileName, Buffer.concat(reqBody).toString()).then(updatedArr => {
+                            reqBody = [];
+                            resolve(responseJson(updatedArr, methodName, fileName, null));
+                        })
+                    });
+                case 'mute':
+                    req.on('data', (chunk) => {
+                        reqBody.push(chunk);
+                    }).on('end', () => {
+                        addToMuteList(fileName, Buffer.concat(reqBody).toString()).then(updatedArr => {
+                            reqBody = [];
+                            resolve(responseJson(updatedArr, methodName, fileName, null));
+                        })
+                    });
+                case 'jail':
+                    req.on('data', (chunk) => {
+                        reqBody.push(chunk);
+                    }).on('end', () => {
+                        addToJailList(fileName, Buffer.concat(reqBody).toString()).then(updatedArr => {
+                            reqBody = [];
                             resolve(responseJson(updatedArr, methodName, fileName, null));
                         })
                     });
                 default:
                     resolve();
             }
-        } else {
+        } else { // Method GET
             if (Object.keys(paramRequest).indexOf('name') !== -1) {
                 // only goboue.json for now
                 loadFile(req.rawHeaders[indexFile + 1], true).then(() => {
@@ -71,6 +90,14 @@ function processRequest(req, res, indexMethod, indexFile, paramRequest) {
                 }).catch(err => {
                     console.log(err);
                     res.write("An error has occured");
+                    res.end();
+                });
+            } else if (Object.keys(paramRequest).indexOf('idUser') !== -1) {
+                loadFile(req.rawHeaders[indexFile + 1], true).then(() => {
+                    resolve(responseJson(tabData, methodName, fileName, paramRequest.idUser));
+                }).catch(err => {
+                    console.log(err);
+                    res.write("An error has occured in unMute function");
                     res.end();
                 });
             } else {
@@ -124,10 +151,28 @@ async function loadFile(name, isJSON) {
     })
 }
 
-function getUpdatedGoboueArray(fileName, element) {
+function addToGoboueArray(fileName, element) {
     return new Promise(resolve => {
         loadFile(fileName, true).then(data => {
             data.scoreChannels.push(JSON.parse(element));
+            resolve(data);
+        });
+    })
+}
+
+function addToMuteList(fileName, element) {
+    return new Promise(resolve => {
+        loadFile(fileName, true).then(data => {
+            data.muteList.push(JSON.parse(element));
+            resolve(data);
+        });
+    })
+}
+
+function addToJailList(fileName, element) {
+    return new Promise(resolve => {
+        loadFile(fileName, true).then(data => {
+            data.jailList.push(JSON.parse(element));
             resolve(data);
         });
     })
@@ -150,6 +195,32 @@ function responseJson(tablal, method, name, params) {
     }
 
     switch (name) {
+        case 'muteList.json':
+            switch (method) {
+                case 'all':
+                    return JSON.stringify(tablal);
+                case 'mute':
+                    return save(tablal, name);
+                case 'unMute':
+                    tablal.muteList = Array.from(tablal.muteList).filter(member => member.id !== params);
+                    return save(tablal, name);
+                default:
+                    return "An error has been found in the method";
+            }
+            break;
+        case 'jailList.json':
+            switch (method) {
+                case 'all':
+                    return JSON.stringify(tablal);
+                case 'jail':
+                    return save(tablal, name);
+                case 'unJail':
+                    tablal.jailList = Array.from(tablal.jailList).filter(member => member.id !== params);
+                    return save(tablal, name);
+                default:
+                    return "An error has been found in the method";
+            }
+            break;
         case 'jokes.json':
             switch (method) {
                 case 'all':
@@ -176,35 +247,36 @@ function responseJson(tablal, method, name, params) {
                 default:
                     return "An error has been found in the method";
             }
-            case 'goboue.json':
-                switch (method) {
-                    case 'nameIsUnique':
-                        for (let indexChannel in tablal.scoreChannels) {
-                            if (tablal.scoreChannels[indexChannel].name.toLowerCase() === params.toLowerCase()) {
-                                return JSON.stringify(false);
-                            }
+            break;
+        case 'goboue.json':
+            switch (method) {
+                case 'nameIsUnique':
+                    for (let indexChannel in tablal.scoreChannels) {
+                        if (tablal.scoreChannels[indexChannel].name.toLowerCase() === params.toLowerCase()) {
+                            return JSON.stringify(false);
                         }
-                        return JSON.stringify(true);
-                    case 'findByNom':
-                        for (let indexChannel in tablal.scoreChannels) {
-                            if (tablal.scoreChannels[indexChannel].name.toLowerCase() === params.toLowerCase()) {
-                                return JSON.stringify(tablal.scoreChannels[indexChannel]);
-                            }
+                    }
+                    return JSON.stringify(true);
+                case 'findByNom':
+                    for (let indexChannel in tablal.scoreChannels) {
+                        if (tablal.scoreChannels[indexChannel].name.toLowerCase() === params.toLowerCase()) {
+                            return JSON.stringify(tablal.scoreChannels[indexChannel]);
                         }
-                        return null;
-                    case 'removeByNom':
-                        let updatedArr = [];
-                        for (let indexChannel in tablal.scoreChannels) {
-                            if (tablal.scoreChannels[indexChannel].name.toLowerCase() !== params.toLowerCase()) {
-                                updatedArr.push(tablal.scoreChannels[indexChannel]);
-                            }
+                    }
+                    return null;
+                case 'removeByNom':
+                    let updatedArr = [];
+                    for (let indexChannel in tablal.scoreChannels) {
+                        if (tablal.scoreChannels[indexChannel].name.toLowerCase() !== params.toLowerCase()) {
+                            updatedArr.push(tablal.scoreChannels[indexChannel]);
                         }
-                        tablal.scoreChannels = updatedArr;
-                        return save(tablal, name);
-                    default:
-                        return "An error has been found in the file name";
-                }
-
+                    }
+                    tablal.scoreChannels = updatedArr;
+                    return save(tablal, name);
+                default:
+                    return "An error has been found in the file name";
+            }
+            break;
     }
 }
 
